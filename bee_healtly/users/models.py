877c907e1aba359ab211368a4b65e73bcc4f4b83
naiarar <rnaiara92@gmail.com>
2023-03-21 19:30:26 -0300

    models user and measurements

--- bee_healtly/users/models.py
+++ bee_healtly/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField(blank=False, null=False)
+    adress = models.CharField(max_length=200,blank=False, null=False)
+    age = models.IntegerField(blank=False, null=False)
+  
+
+    def __str__(self):
+        return self.name
+class Measurements(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='measurements')
+    height = models.DecimalField(max_digits=3, decimal_places=2, blank=False, null=False)
+    weight = models.DecimalField(max_digits=4, decimal_places=1, blank=False, null=False)
+    arms = models.DecimalField(max_digits=4, decimal_places=1, blank=False, null=False) #braço
+    chest = models.DecimalField(max_digits=4, decimal_places=1, blank=False, null=False) #peito
+    waist = models.DecimalField(max_digits=4, decimal_places=1, blank=False, null=False) #cintura
+    hips = models.DecimalField(max_digits=4, decimal_places=1, blank=False, null=False) #quadril
+    legs = models.DecimalField(max_digits=4, decimal_places=1, blank=False, null=False) #perna
+
