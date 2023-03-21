e6f00b1bd496d756624208dccf6aa7a7d0b74925
naiarar <rnaiara92@gmail.com>
2023-03-21 18:13:57 -0300

    views from users

--- bee_healtly/users/views.py
+++ bee_healtly/users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def user(request):
+    return HttpResponse('Página Geral Usuario')
+
+
+def user_detail(request, id):
+    return HttpResponse(id)
+
+def user_name(request, nome):
+    return HttpResponse(f' Olá {nome}')
