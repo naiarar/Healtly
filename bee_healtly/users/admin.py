7107af322cad7fc812c79a5da883ff2d1c75e0bf
naiarar <rnaiara92@gmail.com>
2023-03-22 16:31:01 -0300

    adc user admin for organizate admin

--- bee_healtly/users/admin.py
+++ bee_healtly/users/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import User, Measurements, Training_level
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'age', 'training_level')
+    list_filter = ('training_level',)
+    search_fields = ('name',)
 
 
-admin.site.register(User)
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Measurements)
 admin.site.register(Training_level)
