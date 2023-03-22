59c0050fefadb385343d50b81503b2f29360c287
naiarar <rnaiara92@gmail.com>
2023-03-22 15:42:11 -0300

    register training level

--- bee_healtly/users/admin.py
+++ bee_healtly/users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Measurements
+from .models import User, Measurements, Training_level
 
 
 
 admin.site.register(User)
 admin.site.register(Measurements)
+admin.site.register(Training_level)
