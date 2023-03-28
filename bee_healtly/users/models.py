d25eedad51c7a27c6f425efb1da9cb147270db76
naiarar <rnaiara92@gmail.com>
2023-03-28 18:37:42 -0300

    migrations date

--- bee_healtly/users/models.py
+++ bee_healtly/users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 
 class Training_level(models.Model):
     level = models.CharField(max_length=20)
@@ -8,11 +8,11 @@ class Training_level(models.Model):
         return self.level
 
 
-
 class User(models.Model):
     name = models.CharField(max_length=50, blank=False, null=False)
     email = models.EmailField(blank=False, null=False)
     adress = models.CharField(max_length=200,blank=False, null=False)
+    date_of_birth = models.CharField(max_length=20, blank=False)
     age = models.IntegerField(blank=False, null=False)
     profile_picture = models.ImageField(upload_to='users_pictures', blank=True, null=True)
     training_level = models.ForeignKey(Training_level, on_delete=models.DO_NOTHING, blank=True)
@@ -20,7 +20,10 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
 class Measurements(models.Model):
+    date = models.DateField(default=timezone.now)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='measurements')
     height = models.DecimalField(max_digits=3, decimal_places=2, blank=False, null=False)
     weight = models.DecimalField(max_digits=4, decimal_places=1, blank=False, null=False)
@@ -30,3 +33,6 @@ class Measurements(models.Model):
     hips = models.DecimalField(max_digits=4, decimal_places=1, blank=False, null=False) #quadril
     legs = models.DecimalField(max_digits=4, decimal_places=1, blank=False, null=False) #perna
     body_pictures = models.ImageField(upload_to='users_pictures', blank=True)
+
+    def __str__(self):
+        return self.user.name
